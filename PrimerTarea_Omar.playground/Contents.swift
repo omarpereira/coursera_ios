8776e6d435c89f2a5292d0b00c03b80e7de0b319
Desarrollo <desarrollo@desarrollo.local>
2016-08-19 20:09:40 -0500

    Primera version

---
+++ PrimerTarea_Omar.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/* Omar Ulises Pereira 
+ 
+ Primera Tarea  
+ 
+ Coursera IOS  */
+
+for rango in 0...100
+{
+    if rango % 5 == 0 { print("\(rango)  " + "Bingo!!!") }
+    if rango % 2 == 0 { print("\(rango)  " + "Par!!!") }
+    if rango % 2 != 0 { print("\(rango)  " + "Impar!!!") }
+    if rango >= 30 && rango <= 40 { print("\(rango)" + "Viva Switf!!!") }
+    
+}
+
+
